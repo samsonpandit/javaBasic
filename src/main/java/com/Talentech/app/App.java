01dd26c87841a5499d8be3bb95b32fdf748a7dc4
samson6666pandit <somi_pandit@yahoo.com>
2017-09-12 14:04:16 -0400

    first commit

---
+++ src/main/java/com/Talentech/app/App.java
@@ -0,0 +1,13 @@
+package com.Talentech.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
